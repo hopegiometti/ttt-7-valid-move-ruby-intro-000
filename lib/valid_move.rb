7481590ae3042aef31de11f1d255eece0c3ac19c
Hope Giometti <hopegiometti@gmail.com>
2019-07-03 18:52:11 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 #valid_move?
 def valid_move?(board, index)
-  if index.between?(1, 9)
+  if index.between?(1, 9) && board[index] == " "
     return true
   end
 end
